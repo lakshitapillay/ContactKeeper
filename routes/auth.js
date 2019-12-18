2e3c2fef71ca4685427da6426b216283441e71ab
lakshitapillay <pillaylakshita@gmail.com>
2019-12-18 16:12:44 +0530

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Auth')
+});
+
+router.post('/', (req, res) => {
+    res.send('Auth')
+});
+
+module.exports = router;
