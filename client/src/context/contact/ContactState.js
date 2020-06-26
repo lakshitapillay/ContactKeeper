ee9bf8444ac2578d3ec348bcc6742f48659d76fb
lakshitapillay <pillaylakshita@gmail.com>
2020-06-26 20:15:55 +0530

    backend and frontend done(not yet connected)

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,80 @@
+import React, { useReducer } from 'react'
+import uuid from 'uuid'
+import ContactContext from './contactContext'
+import contactReducer from './contactReducer'
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_CONTACTS,
+    CLEAR_FILTER
+} from '../types'
+
+const ContactState = props => {
+    const initialState = {
+        contacts: [
+            {
+                id: 1,
+                name: 'lak',
+                email: 'pillaylakshita@gmail.com',
+                phone: '9167215174',
+                type: 'personal'
+            },
+            {
+                id: 2,
+                name: 'lak2',
+                email: 'pillaylakshita1@gmail.com',
+                phone: '9167215175',
+                type: 'professional'
+            }
+        ],
+        current: null,
+        filtered: null
+    }
+    const [state, dispatch] = useReducer(contactReducer, initialState);
+
+    const addContact = contact => {
+        contact.id = uuid.v4();
+        dispatch({ type: ADD_CONTACT, payload: contact });
+    }
+    const updateContact = contact => {
+        dispatch({ type: UPDATE_CONTACT, payload: contact });
+    }
+    const deleteContact = id => {
+        dispatch({ type: DELETE_CONTACT, payload: id });
+    }
+    const setCurrent = contact => {
+        dispatch({ type: SET_CURRENT, payload: contact });
+    }
+    const clearCurrent = () => {
+        dispatch({ type: CLEAR_CURRENT });
+    }
+    const filterContacts = text => {
+        dispatch({ type: FILTER_CONTACTS, payload: text });
+    }
+    const clearFilter = () => {
+        dispatch({ type: CLEAR_FILTER });
+    }
+    return (
+        <ContactContext.Provider
+            value={{
+                contacts: state.contacts,
+                current: state.current,
+                filtered: state.filtered,
+                addContact,
+                deleteContact,
+                setCurrent,
+                clearCurrent,
+                updateContact,
+                filterContacts,
+                clearFilter
+            }}
+        >
+            {props.children}
+        </ContactContext.Provider>
+    )
+}
+export default ContactState;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 
 const app = express();
+connectDB();
 const PORT = process.env.PORT || 5000;
 
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contacts', require('./routes/contacts'))
+app.use(express.json({ extender: false }));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 app.listen(PORT, () => console.log(`server started on ${PORT}`));
