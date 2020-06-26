7f5e30226fee2209c19d1b0d47d4e9c9500c26f4
lakshitapillay <pillaylakshita@gmail.com>
2020-06-26 22:01:13 +0530

    auth context and alert context

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,35 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from '../src/components/layouts/Navbar'
 import Home from '../src/components/pages/Home'
 import About from '../src/components/pages/About'
+import { Register } from '../src/components/auth/Register'
+import { Login } from '../src/components/auth/Login'
+import { Alerts } from '../src/components/layouts/Alerts'
 import ContactState from './context/contact/ContactState'
+import AuthState from './context/auth/AuthState'
+import AlertState from './context/alert/AlertState'
 
 function App() {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
   );
 }
 
--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -13,6 +13,12 @@ export default function Navbar({ title, icon }) {
                 <li>
                     <Link to='/about'>About</Link>
                 </li>
+                <li>
+                    <Link to='/login'>Login</Link>
+                </li>
+                <li>
+                    <Link to='/register'>Register</Link>
+                </li>
             </ul>
         </div>
     )
